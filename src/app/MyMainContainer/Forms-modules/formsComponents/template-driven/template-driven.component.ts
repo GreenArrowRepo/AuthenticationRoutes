eb75918399160ed05ffd639d88de223527b9509c
GreenArrowRepo <hims.mehuriya@gmail.com>
2021-09-09 17:50:14 +0530

    Revert "Setup sign up page"
    This reverts commit 683ed3b39f78125eb06fff34ae0300136f7decff.

--- src/app/MyMainContainer/Forms-modules/formsComponents/template-driven/template-driven.component.ts
+++ src/app/MyMainContainer/Forms-modules/formsComponents/template-driven/template-driven.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit, ViewChild } from '@angular/core';
-import { FormBuilder, FormGroup, NgForm, Validators } from '@angular/forms';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-template-driven',
@@ -8,37 +8,73 @@ import { FormBuilder, FormGroup, NgForm, Validators } from '@angular/forms';
 })
 export class TemplateDrivenComponent implements OnInit {
 
-  
+  @ViewChild('myForm') myFormViewChild!: NgForm | undefined;
 
-  form: any;
-
-  constructor(private fb : FormBuilder) { 
+  constructor() { 
     console.log("template driven module loaded.")
   }
 
   ngOnInit(): void {
-
-    this.form = this.fb.group({
-
-      email:['',[Validators.required, Validators.email]],
-      password:['',[Validators.required, Validators.minLength(6)]]
-    })
   }
-  
-  
+  defaultCourse : string = "HTML";
+  defaultGender : string = "Male";
   enteredUname : string = "";
   submitted : boolean = false;
-   
+  radioButton : any = [
+    {id: '1', gender : 'Male'},
+    {id: '2', gender : 'Female'},
+  ] 
 
-  
+  formData : any = [
+    {username : ''},
+    {emailid : ''},
+    {course : ''},
+    {gender : ''}
+
+  ]
 
   onSubmit()
   {
     //console.log(formReceived) 
-    console.log(this.form.value);
+    console.log(this.myFormViewChild);
     this.submitted = true;
-    
-    
+
+    this.formData.username = this.myFormViewChild?.value.userdetail.nameUname;
+    this.formData.emailid = this.myFormViewChild?.value.userdetail.NameEmail;
+    this.formData.course = this.myFormViewChild?.value.nameCourses;
+    this.formData.gender = this.myFormViewChild?.value.flexRadioDefault;
+
+    this.myFormViewChild?.reset();
+  }
+
+
+  setUserDetails()
+  {
+    this.myFormViewChild?.setValue(
+      {
+        userdetail : 
+        {
+          nameUname : 'Learningmate',
+          NameEmail : "himanshu.maisuriya@learningmate.com"
+        },
+        namePsw : "",
+        nameCourses:   "Angular",
+        flexRadioDefault:  "Male"
+      }
+
+    )
+  }
+
+  patchUserDetails()
+  {
+    this.myFormViewChild?.form.patchValue (
+      {
+        userdetail : 
+        {
+          nameUname : 'Patched',
+        },
+      }
+    )
   }
 
 
